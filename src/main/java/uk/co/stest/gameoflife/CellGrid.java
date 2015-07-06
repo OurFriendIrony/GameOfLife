223fc78659b47bdebf878b7cec3ba11c4dc080da
Steve Stevens <Steve.Stevens@live.co.uk>
2015-07-06 23:40:33 +0100

    grid has control over life of cell

---
+++ src/main/java/uk/co/stest/gameoflife/CellGrid.java
@@ -0,0 +1,39 @@
+package uk.co.stest.gameoflife;
+
+public class CellGrid {
+
+    private final int width;
+    private final int height;
+
+    private Cell[][] cells;
+
+    public CellGrid() {
+        this(3, 3);
+    }
+
+    public CellGrid(int height, int width) {
+        this.height = height;
+        this.width = width;
+
+        cells = new Cell[height][width];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                cells[i][j] = new Cell();
+    }
+
+    public int getWidth() {
+        return cells[0].length;
+    }
+
+    public int getHeight() {
+        return cells.length;
+    }
+
+    public Cell getCell(int x, int y) {
+        return cells[x - 1][y - 1];
+    }
+
+    public void birthCell(int x, int y) {
+        cells[x - 1][y - 1].live();
+    }
+}
---
+++ src/test/java/uk/co/stest/gameoflife/CellGridTest.java
@@ -0,0 +1,63 @@
+package uk.co.stest.gameoflife;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CellGridTest {
+
+    @Test
+    public void cellGridsAreDefaultThreeByThree() {
+        CellGrid grid = new CellGrid();
+
+        assertThat(grid.getHeight()).isEqualTo(3);
+        assertThat(grid.getWidth()).isEqualTo(3);
+    }
+
+    @Test
+    public void cellGridsSizeIsDefinedByConstructor() {
+        CellGrid grid = new CellGrid(1, 4);
+
+        assertThat(grid.getHeight()).isEqualTo(1);
+        assertThat(grid.getWidth()).isEqualTo(4);
+    }
+
+    @Test
+    public void gridsHaveCells() {
+        CellGrid grid = new CellGrid(1, 1);
+
+        assertThat(grid.getCell(1, 1)).isInstanceOf(Cell.class);
+    }
+
+    @Test
+    public void gridsAreCreatedBarren() {
+        CellGrid grid = new CellGrid();
+
+        assertThat(grid.getCell(1, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(3, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(1, 2).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 2).isAlive()).isFalse();
+        assertThat(grid.getCell(3, 2).isAlive()).isFalse();
+        assertThat(grid.getCell(1, 3).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 3).isAlive()).isFalse();
+        assertThat(grid.getCell(3, 3).isAlive()).isFalse();
+    }
+
+    @Test
+    public void canBirthSpecificCells() {
+        CellGrid grid = new CellGrid();
+
+        grid.birthCell(2, 2);
+
+        assertThat(grid.getCell(1, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(3, 1).isAlive()).isFalse();
+        assertThat(grid.getCell(1, 2).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 2).isAlive()).isTrue();
+        assertThat(grid.getCell(3, 2).isAlive()).isFalse();
+        assertThat(grid.getCell(1, 3).isAlive()).isFalse();
+        assertThat(grid.getCell(2, 3).isAlive()).isFalse();
+        assertThat(grid.getCell(3, 3).isAlive()).isFalse();
+    }
+}
