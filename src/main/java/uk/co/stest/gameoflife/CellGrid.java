53ab06bdd01d3f8bf9af4f87621c0885f2aa3d17
Steve Stevens <Steve.Stevens@live.co.uk>
2015-07-09 05:25:43 +0100

    refactor grid constructor

--- src/main/java/uk/co/stest/gameoflife/CellGrid.java
+++ src/main/java/uk/co/stest/gameoflife/CellGrid.java
@@ -9,19 +9,22 @@ public class CellGrid {
     }
 
     public CellGrid(int height, int width) {
-
         cells = new Cell[height][width];
+        populateGrid(height, width);
+    }
+
+    private void populateGrid(int height, int width) {
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++)
                 cells[i][j] = new Cell();
     }
 
-    public int getWidth() {
-        return cells[0].length;
-    }
-
     public int getHeight() {
         return cells.length;
+    }
+
+    public int getWidth() {
+        return cells[0].length;
     }
 
     public Cell getCell(int x, int y) {
